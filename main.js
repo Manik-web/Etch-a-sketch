58b65227c36945ac00914e6958bd475aeb794fd5
Manik-web <sundeepmanik83@gmail.com>
2020-09-13 19:36:57 +0100

    basic 4x4 grid added

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
 console.log('hi');
+let container = document.querySelector('#container');
+
+
+
+
+
+let grid1 = document.createElement('div');
+let grid2 = document.createElement('div');
+let grid3 = document.createElement('div');
+let grid4 = document.createElement('div');
+let grid5 = document.createElement('div');
+let grid6 = document.createElement('div');
+let grid7 = document.createElement('div');
+let grid8 = document.createElement('div');
+let grid9 = document.createElement('div');
+let grid10 = document.createElement('div');
+let grid11 = document.createElement('div');
+let grid12 = document.createElement('div');
+let grid13 = document.createElement('div');
+let grid14 = document.createElement('div');
+let grid15 = document.createElement('div');
+let grid16 = document.createElement('div');
+
+container.append(grid1);
+container.append(grid2);
+container.append(grid3);
+container.append(grid4);
+container.append(grid5);
+container.append(grid6);
+container.append(grid7);
+container.append(grid8);
+container.append(grid9);
+container.append(grid10);
+container.append(grid11);
+container.append(grid12);
+container.append(grid13);
+container.append(grid14);
+container.append(grid15);
+container.append(grid16);
