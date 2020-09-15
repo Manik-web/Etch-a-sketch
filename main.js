ced9baaefe1acb41ce296636dd7e645794668207
Manik-web <sundeepmanik83@gmail.com>
2020-09-15 18:01:01 +0100

    etching done, button task remaining

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 console.log('hi');
 let container = document.querySelector('#container');
 
-
-
-
-
 let grid1 = document.createElement('div');
 let grid2 = document.createElement('div');
 let grid3 = document.createElement('div');
@@ -22,19 +18,41 @@ let grid14 = document.createElement('div');
 let grid15 = document.createElement('div');
 let grid16 = document.createElement('div');
 
+turnOn =(event)=>{
+    
+    event.target.style.background='lightgrey';
+}
+
+
 container.append(grid1);
+grid1.addEventListener("mouseover", turnOn);
 container.append(grid2);
+grid2.addEventListener('mouseover',turnOn);
 container.append(grid3);
+grid3.addEventListener('mouseover',turnOn);
 container.append(grid4);
+grid4.addEventListener('mouseover',turnOn);
 container.append(grid5);
+grid5.addEventListener('mouseover',turnOn);
 container.append(grid6);
+grid6.addEventListener('mouseover',turnOn);
 container.append(grid7);
+grid7.addEventListener('mouseover',turnOn);
 container.append(grid8);
+grid8.addEventListener('mouseover',turnOn);
 container.append(grid9);
+grid9.addEventListener('mouseover',turnOn);
 container.append(grid10);
+grid10.addEventListener('mouseover',turnOn);
 container.append(grid11);
+grid11.addEventListener('mouseover',turnOn);
 container.append(grid12);
+grid12.addEventListener('mouseover',turnOn);
 container.append(grid13);
+grid13.addEventListener('mouseover',turnOn);
 container.append(grid14);
+grid14.addEventListener('mouseover',turnOn);
 container.append(grid15);
+grid15.addEventListener('mouseover',turnOn);
 container.append(grid16);
+grid16.addEventListener('mouseover',turnOn);
