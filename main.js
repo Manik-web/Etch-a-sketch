78b0fdc91fe0182cfbc57318b6fe65934a6f99af
Manik-web <sundeepmanik83@gmail.com>
2020-09-18 21:50:11 +0100

    reset working, and dynamic sizing in, but very slow

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 console.log('hi');
 let container = document.querySelector('#container');
+let htmlStyles = window.getComputedStyle(document.querySelector("html"));
+let rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
+let rowWidth = parseInt(htmlStyles.getPropertyValue("--rowWidth"));
+let fullWidth = 800;
+
 
 turnOn =(event)=>{
     if(event.target.style.background==='lightgrey'){
@@ -10,6 +15,11 @@ turnOn =(event)=>{
 }
 
 let makeGrid =(width)=>{
+    rowWidth = 1000/width;
+    container.innerHTML = '';
+    container.style.display = 'grid';
+    document.documentElement.style.setProperty("--rowNum", width);
+    document.documentElement.style.setProperty("--rowWidth", rowWidth+'px');
     let number = width*width;
     for (i=0;i<number;i++){
         container.innerHTML +='<div class="grid" id="grid'+(i+1)+'"></div';
@@ -18,8 +28,9 @@ let makeGrid =(width)=>{
     Array.from(elements).forEach(function(element) {
         element.addEventListener('mouseover', turnOn);
       });
+
 }
-makeGrid(4);
+makeGrid(10);
 
 
 
@@ -28,6 +39,8 @@ let reset = () =>{
     for(j=0;j<e.length;j++){
         e[j].style.background = 'white';
     }
+    let size= prompt('please enter the desired width (MAX is 64)');
+    makeGrid(size);
 }
 
 let button = document.getElementById("button");
